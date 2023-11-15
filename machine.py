3430bdf4e0766f6d56395620627ed33f533d2104
nick-onetap <xtrpoptart@gmail.com>
2023-11-15 12:38:46 -0500

    Spinning animation

--- machine.py
+++ machine.py
@@ -7,8 +7,36 @@ class Machine:
         self.display_surface = pygame.display.get_surface()
         self.reel_index = 0
         self.reel_list = {}
+        self.can_toggle = True
+        self.spinning = False
 
         self.spawn_reels()
+
+    # Spins only if all reels are not spinning
+    def cooldowns(self):
+        for reel in self.reel_list:
+            if self.reel_list[reel].reel_is_spinning:
+                self.can_toggle = False
+                self.spinning = True
+        
+        if not self.can_toggle and [self.reel_list[reel].reel_is_spinning for reel in self.reel_list].count(False) == 5:
+            self.can_toggle = True
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        # Checking for space key, toggle spin ability, balance for bet sizing
+        #if keys[pygame.K_SPACE] and self.can_toggle and self.currPLayer.balance >= self.currPlayer.bet_size:
+        if keys[pygame.K_SPACE]:
+            self.toggle_spinning()
+            self.spin_time = pygame.time.get_ticks()
+            #self.currPlayer.place_bet()
+            #self.machine_balance += self.currPlayer.bet_size
+            #self.currPLayer.last_payout = None
+
+    def draw_reels(self, delta_time):
+        for reel in self.reel_list:
+            self.reel_list[reel].animate(delta_time)
 
     def spawn_reels(self):
         if not self.reel_list:
@@ -19,7 +47,20 @@ class Machine:
             self.reel_list[self.reel_index] = Reel((x_topleft, y_topleft))
             self.reel_index += 1
 
+    def toggle_spinning(self):
+        if self.can_toggle:
+            self.spin_time = pygame.time.get_ticks()
+            self.spinning = not self.spinning
+            self.can_toggle = False
+
+            for reel in self.reel_list:
+                # Spin Delay
+                self.reel_list[reel].start_spin(int(reel) * 350)
+                #self.spin_sound.play()
+
     def update(self, delta_time):
+        self.input()
+        self.draw_reels(delta_time)
         for reel in self.reel_list:
             self.reel_list[reel].symbol_list.draw(self.display_surface)
             self.reel_list[reel].symbol_list.update()
--- reel.py
+++ reel.py
@@ -11,13 +11,47 @@ class Reel:
         self.reel_is_spinning = False
 
         # Sounds
-        # Go here
+        #self.stop_sound = pygame.mixer.Sound('audio/stop.mp3')
+        #self.stop_sound.set_volume(0.5)
 
         for idx, item in enumerate(self.shuffled_keys):
             self.symbol_list.add(Symbol(symbols[item], pos, idx))
             pos = list(pos)
             pos[1] += 300
             pos = tuple(pos)
+
+    def animate(self, delta_time):
+        if self.reel_is_spinning:
+            self.delay_time -= (delta_time * 1000)
+            self.spin_time -= (delta_time * 1000)
+            reel_is_stopping = False
+
+            if self.spin_time < 0:
+                reel_is_stopping = True
+
+            # Staggers reels
+            if self.delay_time <= 0:
+
+                # Creates new symbol at top of stack
+                for symbol in self.symbol_list:
+                    symbol.rect.bottom += 100
+
+                    # Spacing dependant on when the above statement hits 1200
+                    if symbol.rect.top == 1200:
+                        if reel_is_stopping:
+                            self.reel_is_spinning = False
+                            #self.stop_sound.play()
+
+                        symbol_idx = symbol.idx
+                        symbol.kill()
+                        # Top symbol spawn
+                        self.symbol_list.add(Symbol(symbols[random.choice(self.shuffled_keys)], ((symbol.x_val), -300), symbol_idx))
+
+    def start_spin(self, delay_time):
+        self.delay_time = delay_time
+        # Spin time
+        self.spin_time = 1500 + delay_time
+        self.reel_is_spinning = True
 
 class Symbol(pygame.sprite.Sprite):
     def __init__(self, pathToFile, pos, idx):
