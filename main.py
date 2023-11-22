6d0692ac4f8bb0540c4f058919f85420112c4baa
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2023-11-22 14:47:34 -0500

    Compatibility edits

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from machine import Machine
 from settings import *
-import pygame, sys
+import pygame, sys, asyncio
 
 class Game:
     def __init__(self):
@@ -23,7 +23,7 @@ class Game:
         main_sound = pygame.mixer.Sound('audio/bgmusic.mp3')
         main_sound.play(loops = -1)
 
-    def run(self):
+    async def run(self):
 
         self.start_time = pygame.time.get_ticks()
 
@@ -42,7 +42,8 @@ class Game:
             self.screen.blit(self.bg_image, (0, 0))
             self.machine.update(self.delta_time)
             self.clock.tick(FPS)
+            asyncio.sleep(0)
 
 if __name__ == '__main__':
     game = Game()
-    game.run()
+    asyncio.run(game.run())
